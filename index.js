185f890a0fac4906ca0e54e448d9a23c4c86541e
ekwunifeifenna <ifennaekwunife73@gmail.com>
2023-12-18 17:37:12 -0500

    Add the heading

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ let secondCard = 7
 
 let sum = firstCard + secondCard
 
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
 if (sum <= 20) {
-    console.log("Do you want to draw a new card?")
+    message = "Do you want to draw a new card?"
 } else if (sum === 21){
-    console.log("You've got Blackjack!!")
+    message = "You've got Blackjack!!"
+    hasBlackJack = true
 } else {
-    console.log("You are out of the game")
+    message = "You are out of the game"
+    isAlive = false
 }
+
+
+//cash out your money
