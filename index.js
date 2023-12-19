4432d125395eaa3e40b18786be449b0f6f225093
ekwunifeifenna <ifennaekwunife73@gmail.com>
2023-12-18 21:53:37 -0500

    Render all the important components to the page The message The card P The sum P The start game button Add some styling

--- index.js
+++ index.js
@@ -10,15 +10,29 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21){
-    message = "You've got Blackjack!!"
-    hasBlackJack = true
-} else {
-    message = "You are out of the game"
-    isAlive = false
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+function startGame() {
+    cardsEl.textContent =  "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21){
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You are out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
+
 }
+
+
 
 
 //cash out your money
